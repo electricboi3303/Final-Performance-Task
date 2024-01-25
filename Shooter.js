179a24c522f26f2f68da92253464943423201761
electricboi3303 <77820953+electricboi3303@users.noreply.github.com>
2024-01-25 07:47:39 -0500

    Update Shooter.js

--- Shooter.js
+++ Shooter.js
@@ -1,10 +1,10 @@
 class Shooter{
     constructor(_x, _y, _type, _image){
-        let swidth = 100;
-        let sheight = 100;
+        let swidth = 100; //width of shooter
+        let sheight = 100; //height of shooter
 
-        this.pos = createVector(_x, _y);
-        this.center = createVector(_x - 50, _y - 50);
+        this.pos = createVector(_x, _y); //top left corner of shooter
+        this.center = createVector(_x - swidth / 2, _y - sheight/2); //center of shooter
         this.type = _type;
         this.image = _image;
         this.frames = 100;
@@ -18,7 +18,7 @@ class Shooter{
 
     update(){
         this.frames++;
-        if(this.frames % attackspeeds[this.type] == 0 && inrange.length > 0){ //determines attack speed in terms of number of frames, e.g. type 0 tower will shoot once ever 100 frames
+        if(this.frames % attackspeeds[this.type] == 0 && inrange.length > 0){ //determines attack speed in terms of number of frames, e.g. type 0 tower will shoot once every 500 frames
             bullets.push(new Bullet(this.center.x + 50, this.center.y + 50, this.type, this.target, bulletimage));
         } 
     }
