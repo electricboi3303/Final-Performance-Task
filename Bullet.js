e4ea2d64dbfafcfbc2164978aff340fe835a3dc4
electricboi3303 <77820953+electricboi3303@users.noreply.github.com>
2024-01-25 07:42:25 -0500

    Update Bullet.js

--- Bullet.js
+++ Bullet.js
@@ -2,11 +2,11 @@ class Bullet{
     constructor(_x, _y, _type, _enemy, _image){
         this.pos = createVector(_x, _y);
         this.vel = createVector(0, 0);
-        this.type = _type;
-        this.enemy = _enemy;
-        this.image = _image;
-        this.tpos = createVector(_x, _y);
-        this.frames = 0;
+        this.type = _type; //type data for bullet
+        this.enemy = _enemy; //enemy that bullet targets
+        this.image = _image; //bullet image
+        this.tpos = createVector(_x, _y); //position of tower it shoots from
+        this.frames = 0; //framecount
     }
 
     display(){
@@ -15,11 +15,11 @@ class Bullet{
     }
 
     move(){
-        let xdist = this.enemy.pos.x - this.pos.x;
+        let xdist = this.enemy.pos.x - this.pos.x; //distance from enemy position to bullet
         let ydist = this.enemy.pos.y - this.pos.y;
-        let angle = atan2(ydist, xdist);
+        let angle = atan2(ydist, xdist); //angle for bullet to shoot at
 
-        this.vel.x = cos(angle) * bulletspeeds[this.type];
+        this.vel.x = cos(angle) * bulletspeeds[this.type]; //velocity for bullet 
         this.vel.y = sin(angle) * bulletspeeds[this.type];
 
         this.pos.x += this.vel.x;
@@ -27,7 +27,7 @@ class Bullet{
     }
 
     update(){
-        this.frames++;
-        return this.frames % 25 == 0
+        this.frames++; //counts how many frames
+        return this.frames % 25 == 0 //returns true if framecount is 25, used to remove bullets that missed their target and stay on the screen for too long
     }
 }
